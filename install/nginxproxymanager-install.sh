819a5e051ead3b3594cd87068cd05d5f837be15d
Vu <vumt.dev@gmail.com>
2025-10-18 15:27:28 +0700

    Update Node.js version for installation script

--- install/nginxproxymanager-install.sh
+++ install/nginxproxymanager-install.sh
@@ -53,7 +53,7 @@ $STD apt update
 $STD apt -y install openresty
 msg_ok "Installed Openresty"
 
-NODE_VERSION="22" NODE_MODULE="pnpm@latest" setup_nodejs
+NODE_VERSION="20" NODE_MODULE="pnpm@latest" setup_nodejs
 
 RELEASE=$(curl -fsSL https://api.github.com/repos/NginxProxyManager/nginx-proxy-manager/releases/latest |
   grep "tag_name" |
@@ -120,6 +120,7 @@ msg_info "Building Frontend"
 cd ./frontend
 $STD pnpm install
 $STD pnpm upgrade
+$STD npm rebuild node-sass
 $STD pnpm run build
 cp -r dist/* /app/frontend
 cp -r app-images/* /app/frontend/images
